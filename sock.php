d70ec5f823723cbcfb0ff6e7c2f9fced5b2210b2
DIMITA <dimitriomorales01@gmail.com>
2024-02-12 12:42:07 +0100

    live chat implémentation

---
+++ sock.php
@@ -0,0 +1,140 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+// Définir l'adresse et le port du serveur WebSocket
+$adresse = '0.0.0.0'; // L'adresse IP du serveur (0.0.0.0 signifie toutes les adresses disponibles)
+$port = 8088; // Le port sur lequel le serveur écoutera les connexions WebSocket
+
+// Create WebSocket.
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+socket_bind($socket, $adresse, $port);
+socket_listen($socket);
+
+$client = socket_accept($socket);
+// Send WebSocket handshake headers.
+$request = socket_read($client, 5000);
+preg_match('#Sec-WebSocket-Key: (.*)\r\n#', $request, $matches);
+$key = base64_encode(pack(
+    'H*',
+    sha1($matches[1] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
+));
+$headers = "HTTP/1.1 101 Switching Protocols\r\n";
+$headers .= "Upgrade: websocket\r\n";
+$headers .= "Connection: Upgrade\r\n";
+$headers .= "Sec-WebSocket-Version: 13\r\n";
+$headers .= "Sec-WebSocket-Accept: $key\r\n\r\n";
+socket_write($client, $headers, strlen($headers));
+
+
+
+//bind socket to specifie
+//create & add listning socket to the list
+$clients = array($socket);
+
+//start endless loop, so that our script doesn't stop
+while (true) {
+    //manage multipal connections
+    $changed = $clients;
+    //returns the socket resources in $changed array
+    socket_select($changed, $null, $null, 0, 10);
+
+    //check for new socket
+    if (in_array($socket, $changed)) {
+        $socket_new = socket_accept($socket); //accpet new socket
+        $clients[] = $socket_new; //add socket to client array
+
+        $header = socket_read($socket_new, 1024); //read data sent by the socket
+
+        socket_getpeername($socket_new, $ip); //get ip address of connected socket
+        $response = mask(json_encode(array('type' => 'system', 'message' => $ip . ' connected'))); //prepare json data
+        send_message($response); //notify all users about new connection
+
+        //make room for new socket
+        $found_socket = array_search($socket, $changed);
+        unset($changed[$found_socket]);
+    }
+
+    //loop through all connected sockets
+    foreach ($changed as $changed_socket) {
+
+        //check for any incomming data
+        print($changed_socket);
+        while (socket_recv($changed_socket, $buf, 1024, 0) >= 1) {
+            $received_text = unmask($buf); //unmask data
+            print_r($received_text);
+            $tst_msg = json_decode($received_text, true); //json decode 
+            $user_name = $tst_msg['name']; //sender name
+            $user_message = $tst_msg['message']; //message text
+            $user_color = $tst_msg['color']; //color
+
+            //prepare data to be sent to client
+            $response_text = mask(json_encode(array('type' => 'usermsg', 'name' => $user_name, 'message' => $user_message, 'color' => $user_color)));
+            send_message($response_text); //send data
+            break 2; //exist this loop
+        }
+
+        $buf = @socket_read($changed_socket, 1024, PHP_NORMAL_READ);
+        if ($buf === false) { // check disconnected client
+            // remove client for $clients array
+            $found_socket = array_search($changed_socket, $clients);
+            socket_getpeername($changed_socket, $ip);
+            unset($clients[$found_socket]);
+
+            //notify all users about disconnected connection
+            $response = mask(json_encode(array('type' => 'system', 'message' => $ip . ' disconnected')));
+            send_message($response);
+        }
+    }
+}
+// close the listening socket
+socket_close($socket);
+
+function send_message($msg)
+{
+    global $clients;
+    foreach ($clients as $changed_socket) {
+        @socket_write($changed_socket, $msg, strlen($msg));
+    }
+    return true;
+}
+
+
+//Unmask incoming framed message
+function unmask($text)
+{
+    print_r($text);
+    $length = ord($text[1]) & 127;
+    if ($length == 126) {
+        $masks = substr($text, 4, 4);
+        $data = substr($text, 8);
+    } elseif ($length == 127) {
+        $masks = substr($text, 10, 4);
+        $data = substr($text, 14);
+    } else {
+        $masks = substr($text, 2, 4);
+        $data = substr($text, 6);
+    }
+    $text = "";
+    for ($i = 0; $i < strlen($data); ++$i) {
+        $text .= $data[$i] ^ $masks[$i % 4];
+    }
+    return $text;
+}
+
+//Encode message for transfer to client.
+function mask($text)
+{
+    $b1 = 0x80 | (0x1 & 0x0f);
+    $length = strlen($text);
+
+    if ($length <= 125)
+        $header = pack('CC', $b1, $length);
+    elseif ($length > 125 && $length < 65536)
+        $header = pack('CCn', $b1, 126, $length);
+    elseif ($length >= 65536)
+        $header = pack('CCNN', $b1, 127, $length);
+    return $header . $text;
+}
